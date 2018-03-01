5aadfe15189b9bd5a2b0ccb84b535e4876b6db3b
Unknown <sparking02@yahoo.com.mx>
2018-02-28 21:03:20 -0600

    terminado?

--- js/init.js
+++ js/init.js
@@ -1,3 +1,12 @@
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+	  e.preventDefault();
+	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+  });
+
+
+
 /*
 	Ion by TEMPLATED
 	templated.co @templatedco
